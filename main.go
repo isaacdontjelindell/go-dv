d0558fc26d7370b24e890d55004c9e585c664452
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-02-08 14:26:11 -0600

    more refactoring

--- main.go
+++ main.go
@@ -1,137 +1,111 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "net"
-    "time"
-    "strconv"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
 )
 
 func main() {
-    // get this station name and neighbor/cost
-    configLines, err := readLines("conf.ini")
-    if err != nil {
-        fmt.Println("Error reading config file", err.Error())
-        os.Exit(1)
-    }
+	// get this station name and neighbor/cost
+	configLines, err := readLines("conf.ini")
+	if err != nil {
+		fmt.Println("Error reading config file", err.Error())
+		os.Exit(1)
+	}
 
-    station := configLines[0]
-    fmt.Println("Station:", station)
+	station := configLines[0]
+	fmt.Println("Station:", station)
 
-    neighborCountStr := configLines[1]
-    neighborCount, err := strconv.Atoi(neighborCountStr)
-    if err != nil {
-        fmt.Println("Error converting neighborCost to int.", err.Error())
-        os.Exit(1)
-    }
-    fmt.Println("neighborCount:", neighborCount)
+	neighborCountStr := configLines[1]
+	neighborCount, err := strconv.Atoi(neighborCountStr)
+	if err != nil {
+		fmt.Println("Error converting neighborCost to int.", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("neighborCount:", neighborCount)
 
+	neighbors := make([]Node, 0)
+	for i := 2; i < neighborCount*2+1; i += 2 {
+		name := configLines[i]
+		costStr := configLines[i+1]
 
-    neighbors := make([]Node, 0)
-    for i := 2; i<neighborCount*2 + 1; i+=2 {
-        name := configLines[i]
-        costStr := configLines[i+1]
+		cost, err := strconv.Atoi(costStr)
+		if err != nil {
+			fmt.Println("Error converting cost to int.", err.Error())
+			os.Exit(1)
+		}
 
-        cost, err := strconv.Atoi(costStr)
-        if err != nil {
-            fmt.Println("Error converting cost to int.", err.Error())
-            os.Exit(1)
-        }
+		fmt.Printf("Neighbor: %s, Cost: %d\n", name, cost)
 
-        fmt.Printf("Neighbor: %s, Cost: %d\n", name, cost)
+		n := Node{name, "", cost}
 
-        n := Node{name, "", cost}
+		neighbors = append(neighbors, n)
+	}
+	println("")
 
-        neighbors = append(neighbors, n)
-    }
-    println("")
+	// set up the threads
+	quit := make(chan int)
 
-    // set up the threads
-    quit := make(chan int)
+	updateChan := make(chan Update)
 
-    updateChan := make(chan Update)
+	go maintainRoutingTable(quit, updateChan, neighbors)
+	go acceptUpdates(quit, updateChan)
 
-    go maintainRoutingTable(quit, updateChan, neighbors)
-    go acceptUpdates(quit, updateChan)
+	go testClient() // TODO remove
 
-    go testClient() // TODO remove
-
-    <-quit // blocks
+	<-quit // blocks
 }
 
 func maintainRoutingTable(quit chan int, updateChan chan Update, initialTable []Node) {
-    routingTable := make([]Node, 0)
-    routingTable = append(routingTable, initialTable...)
+	routingTable := make([]Node, 0)
+	routingTable = append(routingTable, initialTable...)
 
-    for {
-        update := <-updateChan
-        fmt.Println("[maintainRoutingTable] processing an update...")
-        fmt.Println(update)
-    }
+	for {
+		update := <-updateChan
+		fmt.Println("[maintainRoutingTable] processing an update...")
+		fmt.Println(update)
+	}
 
-    quit <- -1
+	quit <- -1
 }
 
 func acceptUpdates(quit chan int, updateChan chan Update) {
-    fmt.Println("[acceptUpdates] starting listener")
+	fmt.Println("[acceptUpdates] starting listener")
 
-    // accept updates and pass them to maintainRoutingTable
-    LISTEN_IP := net.ParseIP("127.0.0.1")
-    LISTEN_PORT := 1337
+	// accept updates and pass them to maintainRoutingTable
+	LISTEN_IP := net.ParseIP("127.0.0.1")
+	LISTEN_PORT := 1337
 
-    listenAddr := net.UDPAddr{LISTEN_IP, LISTEN_PORT, ""}
-    listener, err := net.ListenUDP("udp", &listenAddr)
-    if err != nil {
-        fmt.Println("[acceptUpdates] error starting update listener!", err.Error())
-        os.Exit(1)
-    }
+	listenAddr := net.UDPAddr{LISTEN_IP, LISTEN_PORT, ""}
+	listener, err := net.ListenUDP("udp", &listenAddr)
+	if err != nil {
+		fmt.Println("[acceptUpdates] error starting update listener!", err.Error())
+		os.Exit(1)
+	}
 
-    for {
-        data := make([]byte, 2048)
-        n, from, err := listener.ReadFromUDP(data) //first param is number of bytes recieved
-        if err != nil {
-            fmt.Println("[acceptUpdates] Error accepting connection!", err.Error())
-            return
-        }
+	for {
+		data := make([]byte, 2048)
+		n, from, err := listener.ReadFromUDP(data) //first param is number of bytes recieved
+		if err != nil {
+			fmt.Println("[acceptUpdates] Error accepting connection!", err.Error())
+			return
+		}
 
-        fmt.Println("[acceptUpdates] recieved a packet from", from)
+		fmt.Println("[acceptUpdates] recieved a packet from", from)
 
-        // unmarshal update struct from data
-        var update Update
-        err = json.Unmarshal(data[:n], &update)
-        if err != nil {
-            fmt.Println("[acceptUpdates] error unmarshaling struct from JSON!", err.Error())
-            os.Exit(1)
-        }
+		// unmarshal update struct from data
+		var update Update
+		err = json.Unmarshal(data[:n], &update)
+		if err != nil {
+			fmt.Println("[acceptUpdates] error unmarshaling struct from JSON!", err.Error())
+			os.Exit(1)
+		}
 
-        updateChan <- update
-    }
+		updateChan <- update //pass the update object to maintainRoutingTable()
+	}
 
-    quit <- -1
+	quit <- -1
 }
-
-
-func testClient() {
-    conn, err := net.Dial("udp",  "127.0.0.1:1337")
-    if err != nil {
-        fmt.Println("[testClient] Error dialing connection.", err.Error())
-    }
-
-    for {
-        time.Sleep(time.Second * 2)
-
-        // build a test update struct
-        testRoutingTable := []Node{Node{"t1", "yoda", 3}, Node{"t2", "yoda", 5}, Node{"t3", "yoda", 7}}
-        update := Update{testRoutingTable, "yoda"}
-
-        u, err := json.Marshal(update)  // u is []byte
-        if err != nil {
-            fmt.Println("[testClient] error marshaling update to JSON", err.Error())
-        }
-
-        conn.Write(u)
-    }
-}
-
--- util.go
+++ util.go
@@ -1,52 +1,79 @@
 package main
 
 import (
-    "bufio"
-    "os"
-    "fmt"
+	"bufio"
+	"fmt"
+	"os"
+    "net"
+    "time"
+    "encoding/json"
 )
 
 // readLines reads a whole file into memory
 // and returns a slice of its lines.
 func readLines(path string) ([]string, error) {
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-    return lines, scanner.Err()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
-
 type Node struct {
-    Name string
-    Route string
-    TotalCost int // int cost + (cost to route)
+	Name      string
+	Route     string
+	TotalCost int // int cost + (cost to route)
 }
 
 func (n Node) String() string {
-    return fmt.Sprintf("Name: %s, Route: %s, Cost: %d", n.Name, n.Route, n.TotalCost)
+	return fmt.Sprintf("Name: %s, Route: %s, Cost: %d", n.Name, n.Route, n.TotalCost)
 }
 
 type Update struct {
-    RoutingTable []Node
-    From string
+	RoutingTable []Node
+	From         string
 }
 
 func (u Update) String() string {
-    var s string
-    s = fmt.Sprintf("From: %s\nRouting Table:\n", u.From)
-    for _, node := range u.RoutingTable {
-        s += fmt.Sprint("  ")
-        s += fmt.Sprintln(node)
-    }
-    return s
+	var s string
+	s = fmt.Sprintf("From: %s\nRouting Table:\n", u.From)
+	for _, node := range u.RoutingTable {
+		s += fmt.Sprint("  ")
+		s += fmt.Sprintln(node)
+	}
+	return s
 }
 
 
+func testClient() {
+	conn, err := net.Dial("udp", "127.0.0.1:1337")
+	if err != nil {
+		fmt.Println("[testClient] Error dialing connection.", err.Error())
+	}
+
+	for {
+		time.Sleep(time.Second * 2)
+
+		// build a test update struct
+		testRoutingTable := []Node{
+			Node{"t1", "yoda", 3},
+			Node{"t2", "yoda", 5},
+			Node{"t3", "yoda", 7},
+		}
+		update := Update{testRoutingTable, "yoda"}
+
+		u, err := json.Marshal(update) // u is []byte
+		if err != nil {
+			fmt.Println("[testClient] error marshaling update to JSON", err.Error())
+		}
+
+		conn.Write(u)
+	}
+}
